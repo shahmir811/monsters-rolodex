b8ade087187e3c09a5400f219c01786011a48934
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2019-12-18 07:27:59 +0500

    Updates

---
+++ precache-manifest.4e9e19c4173637e2a4d9c512fc8c5c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1626d3a71db0f6f5f9dc9c91f7de1032",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0aa962cd8793328b0fe9",
+    "url": "/monsters-rolodex/static/css/main.930b468a.chunk.css"
+  },
+  {
+    "revision": "0c0d218d604853eeba03",
+    "url": "/monsters-rolodex/static/js/2.b687fe75.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monsters-rolodex/static/js/2.b687fe75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0aa962cd8793328b0fe9",
+    "url": "/monsters-rolodex/static/js/main.78fa9441.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
